39ca6f991f547333c431eb7cc0bb83ac6b87feb5
xeusteerapat <xeus085@gmail.com>
2021-09-28 23:52:09 +0700

    :sparkles: [EDIT] apply authorization for create product

---
+++ src/lambda/products/createProduct.ts
@@ -0,0 +1,42 @@
+import { createLogger } from "./../../utils/logger";
+import "source-map-support/register";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler,
+} from "aws-lambda";
+// import { nanoid } from "nanoid";
+
+const logger = createLogger("Get-Products");
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  logger.info("Processing event in GetProducts: ", event);
+
+  const authorizedObject = event.requestContext.authorizer.claims;
+
+  if (
+    authorizedObject["cognito:groups"].toLocaleLowerCase().includes("admin")
+  ) {
+    return {
+      statusCode: 401,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        message: "You are not allowed to create product",
+      }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      message: "Welcome Admin",
+    }),
+  };
+};
