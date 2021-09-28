810f3d5976d7f6fbe0f6457c617b0afb9ff72adf
xeusteerapat <xeus085@gmail.com>
2021-09-28 22:45:05 +0700

    :sparkles: [EDIT] signin function

--- src/lambda/auth/signin.ts
+++ src/lambda/auth/signin.ts
@@ -1,25 +1,67 @@
+import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyHandler,
   APIGatewayProxyResult,
-} from 'aws-lambda';
-import 'source-map-support/register';
-import { createLogger } from './../../utils/logger';
+} from "aws-lambda";
+import "source-map-support/register";
+import { SignInRequest } from "./../../models/signinRequest";
+import { createLogger } from "./../../utils/logger";
 
-const logger = createLogger('Get-Products');
+const logger = createLogger("Signin-User");
+
+const poolData = {
+  UserPoolId: process.env.USER_POOL_ID,
+  ClientId: process.env.USER_POOL_CLIENT_ID,
+};
+
+async function signinUser(body: SignInRequest) {
+  const { email, password } = body;
+
+  const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
+    {
+      Username: email,
+      Password: password,
+    }
+  );
+
+  const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+  const userData = {
+    Username: email,
+    Pool: userPool,
+  };
+
+  const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function (result) {
+      console.log("access token + " + result.getAccessToken().getJwtToken());
+      console.log("id token + " + result.getIdToken().getJwtToken());
+      console.log("refresh token + " + result.getRefreshToken().getToken());
+    },
+    onFailure: function (err) {
+      console.log(err);
+    },
+  });
+}
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  logger.info('Signin user event: ', event);
+  logger.info("Signin user event: ", event);
+
+  const body = JSON.parse(event.body);
+
+  const result = await signinUser(body);
 
   return {
     statusCode: 201,
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({
-      message: 'Signin',
+      result,
     }),
   };
 };
