e5b30015c31393acaac3032ab3310803cb1fcf57
xeusteerapat <xeus085@gmail.com>
2021-09-30 23:25:57 +0700

    :sparkles: [ADD] add upload image handler

---
+++ src/lambda/images/generateUploadUrl.ts
@@ -0,0 +1,47 @@
+import "source-map-support/register";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler,
+} from "aws-lambda";
+import { createLogger } from "../../utils/logger";
+import {
+  getImageSignedUrl,
+  updateAttachmentUrl,
+} from "../../business-logic/product";
+
+const logger = createLogger("Generate-Upload-Product-Url");
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  logger.info(`Processing upload url ${event}`);
+
+  const productId = event.pathParameters.productId;
+
+  if (!productId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: "Missing TodoId parameter",
+      }),
+    };
+  }
+
+  //TODO: Need to check authorized admin before upload url
+
+  const signedUrl: string = await getImageSignedUrl(productId);
+  logger.info(`Retreived signe url image ${signedUrl}`);
+
+  await updateAttachmentUrl(signedUrl, productId);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      uploadUrl: signedUrl,
+    }),
+  };
+};
