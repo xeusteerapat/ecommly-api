218c25d7bfafc5a70631f1cb82d97ba1b9b25a2a
xeusteerapat <xeus085@gmail.com>
2021-10-01 09:43:43 +0700

    :recycle: [EDIT] update create product function

--- src/business-logic/product.ts
+++ src/business-logic/product.ts
@@ -1,15 +1,35 @@
+import { CreateProduct, Product } from "./../models/Product";
 import { ProductAccess } from "./../data-layer/ProductAccess";
 import { createLogger } from "./../utils/logger";
 import { Images } from "../data-layer/ImageAccess";
+import { nanoid } from "nanoid";
 
 const logger = createLogger("Product-Logic");
 const image = new Images();
 const product = new ProductAccess();
 
 /**
+ * Create new product
+ * @param newProduct object from request body
+ */
+export async function createProduct(
+  newProduct: CreateProduct
+): Promise<Product> {
+  logger.info(`Create new product`);
+
+  const newProductItem: Product = {
+    productId: nanoid(),
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    ...newProduct,
+  };
+
+  return await product.createProduct(newProductItem);
+}
+
+/**
  * @param signedUrl
  * @param productId
- * @param adminId
  */
 export async function updateAttachmentUrl(
   signedUrl: string,
@@ -18,7 +38,7 @@ export async function updateAttachmentUrl(
   // Split url, then make use of the first part
   const attachmentUrl: string = signedUrl.split("?")[0];
 
-  logger.info(`Signed URL ${{ attachmentUrl }}`);
+  logger.info("Signed URL", attachmentUrl);
 
   return await product.updateProductImageUrl(attachmentUrl, productId);
 }
