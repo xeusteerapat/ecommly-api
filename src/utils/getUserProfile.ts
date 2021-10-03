05be33d07c02db22dee5a0e46c1770c6c8726483
xeusteerapat <xeus085@gmail.com>
2021-10-03 16:04:54 +0700

    :label: [ADD] add types for jwk

--- src/utils/getUserProfile.ts
+++ src/utils/getUserProfile.ts
@@ -1,3 +1,4 @@
+import { JWKRoot } from "./../models/JWK";
 import * as jwt from "jsonwebtoken";
 import jwkToPem from "jwk-to-pem";
 import axios from "axios";
@@ -6,15 +7,15 @@ import { createLogger } from "./logger";
 const logger = createLogger("Get-User-Profile");
 
 export async function getUserProfile(token: string) {
-  const { data } = await axios.get(
+  logger.info("Get user profile");
+
+  const response = await axios.get(
     `https://cognito-idp.${process.env.REGION}.amazonaws.com/${process.env.USER_POOL_ID}/.well-known/jwks.json`
   );
 
-  logger.info("Check data", data);
+  const jwk: JWKRoot[] = response.data;
 
-  const pem = jwkToPem(data);
-
-  logger.info("Check pem", pem);
+  const pem = jwkToPem(jwk.keys[0]);
 
   const decodedToken = jwt.verify(token, pem, { algorithms: ["RS256"] });
 
