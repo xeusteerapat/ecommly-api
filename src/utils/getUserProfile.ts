9adb1f9c5dbc827fb4ddf8088714a0ff313b6aaa
xeusteerapat <xeus085@gmail.com>
2021-10-03 14:49:06 +0700

    :boom: [EDIT] edit function

--- src/utils/getUserProfile.ts
+++ src/utils/getUserProfile.ts
@@ -5,13 +5,13 @@ import { createLogger } from "./logger";
 
 const logger = createLogger("Get-User-Profile");
 
-export async function getUserProfile(token: any) {
+export async function getUserProfile(token: string) {
   const { data } = await axios.get(
     `https://cognito-idp.${process.env.REGION}.amazonaws.com/${process.env.USER_POOL_ID}/.well-known/jwks.json`
   );
   const pem = jwkToPem(data);
 
-  jwt.verify(token, pem, function (err, decoded) {
+  jwt.verify(token, pem, { algorithms: ["RS256"] }, function (err, decoded) {
     if (err) {
       logger.error("Something wrong", err);
     }
