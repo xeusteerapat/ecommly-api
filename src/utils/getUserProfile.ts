e0efe1d7b255e53171c3918375105d9117604b38
xeusteerapat <xeus085@gmail.com>
2021-10-03 14:15:24 +0700

    :sparkles: [ADD] get user profile function

---
+++ src/utils/getUserProfile.ts
@@ -0,0 +1,21 @@
+import * as jwt from "jsonwebtoken";
+import jwkToPem from "jwk-to-pem";
+import axios from "axios";
+import { createLogger } from "./logger";
+
+const logger = createLogger("Get-User-Profile");
+
+export async function getUserProfile(token: any) {
+  const { data } = await axios.get(
+    `https://cognito-idp.${process.env.REGION}.amazonaws.com/${process.env.USER_POOL_ID}/.well-known/jwks.json`
+  );
+  const pem = jwkToPem(data);
+
+  jwt.verify(token, pem, function (err, decoded) {
+    if (err) {
+      logger.error("Something wrong", err);
+    }
+
+    return decoded;
+  });
+}
