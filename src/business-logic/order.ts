1d21a7d1250513cc085469619ecb52afb06e0318
xeusteerapat <xeus085@gmail.com>
2021-10-04 09:46:51 +0700

    :sparkles: [EDIT] update create order function

--- src/business-logic/order.ts
+++ src/business-logic/order.ts
@@ -7,13 +7,18 @@ const logger = createLogger("Order-Logic");
 const order = new OrderAccess();
 
 /**
- * Create new product
- * @param newProduct object from request body
+ * Create new order
+ * @param newOrder object from request body
+ * @param userId from request headers
  */
-export async function createOrder(newOrder: CreateOrder): Promise<Order> {
+export async function createOrder(
+  newOrder: CreateOrder,
+  userId: string
+): Promise<Order> {
   logger.info(`Create new product`);
 
   const newOrderItem: Order = {
+    userId,
     orderId: nanoid(),
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
